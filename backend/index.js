4c1a3b8e01b545a8e5cb4218d5e68f008adcf420
akanksha282 <akankshajaiswal003@gmail.com>
2025-07-07 12:39:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
 const cors = require('cors');
 const connectDB = require('./db');
 const Contact = require('./model/contact.js');
